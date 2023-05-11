f866313bdf9e1f2a9dbefe202c03ddbf02cb4771
Safoun <safoun.official@gmail.com>
2023-05-11 20:25:06 +0600

    initializing repository for espresso emporium client

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='display-1 text-center'>blah blah</div>
+  </React.StrictMode>
+)
